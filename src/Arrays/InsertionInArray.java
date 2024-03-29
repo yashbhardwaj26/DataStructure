a103a123d7838eaa5570bd3d98d0d394ed773fb5
Yash Bhardwaj <33004136+yashbhardwaj26@users.noreply.github.com>
2024-03-29 12:48:04 +0530

    Arrays

---
+++ src/Arrays/InsertionInArray.java
@@ -0,0 +1,40 @@
+package Arrays;
+
+public class InsertionInArray {
+
+	int[] intArr;
+
+	public InsertionInArray(int size) {
+
+		intArr = new int[size];
+		for (int i = 0; i < intArr.length; i++) {
+			intArr[i] = Integer.MIN_VALUE;
+
+		}
+	}
+
+	public void insertInarray(int index, int value) {
+		try {
+			if (intArr[index] == Integer.MIN_VALUE) {
+
+				intArr[index] = value;
+				System.out.println("Value inserted successfully");
+
+			}
+
+			else {
+
+				System.out.println("Value already present at that index");
+			}
+
+		}
+
+		catch (ArrayIndexOutOfBoundsException e) {
+
+			System.out.println("Invalid index to access array");
+
+		}
+
+	}
+
+}
---
+++ src/Arrays/Main.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		InsertionInArray intArray = new InsertionInArray(5);
+		
+		intArray.insertInarray(1, 10);
+		intArray.insertInarray(2, 20);
+		intArray.insertInarray(1, 10);
+		intArray.insertInarray(6,60);
+		// TODO Auto-generated method stub
+
+	}
+
+}
