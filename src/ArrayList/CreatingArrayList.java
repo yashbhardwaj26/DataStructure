f00127220dfe960c706521bde5da3762a220eb51
Yash Bhardwaj <33004136+yashbhardwaj26@users.noreply.github.com>
2024-04-02 11:23:54 +0530

    ArrayList

---
+++ src/ArrayList/CreatingArrayList.java
@@ -0,0 +1,16 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CreatingArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayList<Number> l = new ArrayList<Number>(Arrays.asList(10,20,30));
+		
+		System.out.println(l);
+	}
+
+}
