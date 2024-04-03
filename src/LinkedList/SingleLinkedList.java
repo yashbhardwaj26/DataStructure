999e5f4bc560fad3ca8fadef7ef044d11fc2f9e9
Yash Bhardwaj <33004136+yashbhardwaj26@users.noreply.github.com>
2024-04-03 13:47:32 +0530

    linkedList

--- src/LinkedList/SingleLinkedList.java
+++ src/LinkedList/SingleLinkedList.java
@@ -19,5 +19,48 @@ public class SingleLinkedList {
 		
 
 	}
+	
+	
+	public void insertNode(int value, int location) {
+		
+		Node n1 = new Node();
+		n1.value= value;
+		if(head == null) {
+			createSingleLinkedListNode(value);
+			return;
+			
+		}
+		
+		else if(location == 0) {
+			n1.next = head ;
+			head = n1;
+			
+		}
+		
+		else if (location >= size) {
+			n1.next= null;
+			tail.next = n1;
+			tail = n1;
+			
+		}
+		
+		else {
+			
+			Node tempNode = head ; 
+			int index = 0;
+			while (index < location-1) {
+				
+				tempNode = tempNode.next;
+				index++;
+			}
+			
+			Node second = new Node();
+			second = tempNode.next;
+			tempNode.next = n1;
+			n1.next= second;
+		}
+		
+		size ++;
+	}
 
 }
